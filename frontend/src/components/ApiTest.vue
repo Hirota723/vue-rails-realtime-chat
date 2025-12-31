46fb87bd1154298dc4748f8cf6d4a58d5709e706
Hirota723 <koki.programming@gmail.com>
2026-01-01 02:24:29 +0900

    feat: フロントとバックの連携

---
+++ frontend/src/components/ApiTest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>ApiTest</h1>
+    <button @click="test">ApiTest</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref("");
+
+const test = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/test");
+    const data = await response.json();
+    message.value = data.message;
+  } catch (error) {
+    console.error("API Error:", error);
+  }
+};
+</script>
