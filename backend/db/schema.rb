eaf076d87d4a6cfbc653298ff3c097614d965b4b
Hirota723 <koki.programming@gmail.com>
2026-08-05 20:42:56 +0900

    feat: messageモデルとマイグレーションを追加し、部屋との関連を定義

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_02_141829) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_05_100245) do
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.integer "room_id", null: false
+    t.string "sender_name"
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "messages", "rooms"
 end
