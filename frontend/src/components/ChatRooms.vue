c19c46a743fe9fcc7d348586020fdfa122ea4d79
Hirota723 <koki.programming@gmail.com>
2026-08-30 23:13:21 +0900

    feat: チャットルーム作成機能を追加

--- frontend/src/components/ChatRooms.vue
+++ frontend/src/components/ChatRooms.vue
@@ -6,6 +6,11 @@
         <router-link :to="`/rooms/${room.id}`">{{ room.name }}</router-link>
       </li>
     </ul>
+    <h3>チャットルーム作成</h3>
+    <input type="text" v-model="newRoomName" />
+    <div>
+      <button @click="createRoom">作成</button>
+    </div>
   </div>
 </template>
 
@@ -19,11 +24,27 @@ interface ChatRoom {
 }
 
 const chatRooms = ref<ChatRoom[]>([]);
+const newRoomName = ref("");
 
 const fetchChatRooms = async () => {
 	try {
 		const response = await axios.get("http://localhost:3000/rooms");
 		chatRooms.value = response.data as ChatRoom[];
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// biome-ignore lint/correctness/noUnusedVariables: used in the template
+const createRoom = async () => {
+	if (newRoomName.value.trim() === "") return;
+
+	try {
+		const response = await axios.post("http://localhost:3000/rooms", {
+			name: newRoomName.value,
+		});
+		chatRooms.value.push(response.data as ChatRoom);
+		newRoomName.value = "";
 	} catch (error) {
 		console.error(error);
 	}
