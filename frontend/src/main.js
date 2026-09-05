83109cd9a89a85a621658cce695e7d9551c1ed3a
Hirota723 <koki.programming@gmail.com>
2026-09-05 22:11:18 +0900

    feat: main.jsを新規作成し、Vueアプリケーションの初期設定を実装

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,16 @@
 import "./assets/main.css";
 
+import ActionCable from "actioncable";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
-
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index.ts";
+
+const cable = ActionCable.createConsumer("ws://localhost:3000/cable");
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.provide("cable", cable);
 app.mount("#app");
